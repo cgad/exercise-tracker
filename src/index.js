f551b3e286d04283c4b0adf858aa756cbd867a3f
cgad <35946778+cgad@users.noreply.github.com>
2019-05-23 13:14:00 -0600

    Remove unused create-react-app code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
+// js file that's loaded along with index.html
+// loads react, App component
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// render app inside #root in index.html
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
